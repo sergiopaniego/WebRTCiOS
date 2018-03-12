8cc029ce22d99b55056630345bc09813d1d61487
sergiopaniego <sergiopaniegoblanco@gmail.com>
2018-03-12 15:44:06 +0100

    Initial Commit

---
+++ WebRTCapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebRTCapp
+//
+//  Created by Sergio Paniego Blanco on 12/03/2018.
+//  Copyright © 2018 Sergio Paniego Blanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
