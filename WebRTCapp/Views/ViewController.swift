528e9f312d1dc65743602abd72ced62d43c34a5f
sergiopaniego <sergiopaniegoblanco@gmail.com>
2018-05-31 11:22:40 +0200

    Unnecesary code delete

---
+++ WebRTCapp/Views/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  WebRTCapp
+//
+//  Created by Sergio Paniego Blanco on 12/03/2018.
+//  Copyright © 2018 Sergio Paniego Blanco. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import WebRTC
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var url: UITextField!
+    @IBOutlet weak var sessionName: UITextField!
+    @IBOutlet weak var participantName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Did Load")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Did Appear")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startSocket(_ sender: UIButton) {
+        print("Start new View!")
+    }
+}
+
